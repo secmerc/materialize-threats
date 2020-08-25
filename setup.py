2051b155599d58829deba321f848dae14ae22fed
Jacob Salassi <33043272+jacob-salassi@users.noreply.github.com>
2020-08-24 18:40:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="materialize-threats",
-    version="1.0.3",
+    version="1.0.4",
     author="Jacob Salassi",
     author_email="author@example.com",
     description="Analyze draw.io data flow diagrams for STRIDE threat classes",
